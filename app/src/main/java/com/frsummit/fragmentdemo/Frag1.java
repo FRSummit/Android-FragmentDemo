87063b9338221a8dd595341586416954a31f777b
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-07-04 22:23:27 +0600

    Fragment Demo

---
+++ app/src/main/java/com/frsummit/fragmentdemo/Frag1.java
@@ -0,0 +1,24 @@
+package com.frsummit.fragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_frag1, container, false);
+    }
+}
